0c93f2c04216add31ca502fa33d6f31c9c287f43
Pedro Shoji <33377462+Raygagz@users.noreply.github.com>
2023-07-13 23:06:26 +0000

    List all

---
+++ dist/model.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var Model;
+(function (Model) {
+    const ra = '2123689';
+    const host = 'https://todo-server-spa-ozyq2qhxqq-rj.a.run.app/api';
+    class ToDoItem {
+        constructor() {
+            this.id = 0;
+            this.description = '';
+        }
+    }
+    class ToDoItemDAO {
+        listAll() {
+            return __awaiter(this, void 0, void 0, function* () {
+                const url = `${host}/${ra}/list`;
+                const response = yield fetch(url);
+                if (response.ok) {
+                    return (yield response.json()).items;
+                }
+                console.error('Server status: ' + JSON.stringify(yield response.json()));
+                throw new Error("Server-side error. Failed to fetch list.");
+            });
+        }
+        insert(item) {
+            return false;
+        }
+    }
+    Model.ToDoItemDAO = ToDoItemDAO;
+})(Model || (Model = {}));
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const dao = new Model.ToDoItemDAO();
+        const result = yield dao.listAll();
+        console.log(result);
+    });
+}
+main().then();
---
+++ src/model.ts
@@ -0,0 +1,41 @@
+namespace Model {
+    const ra = '2123689';
+    const host = 'https://todo-server-spa-ozyq2qhxqq-rj.a.run.app/api';
+
+    class ToDoItem {
+        id: number = 0;
+        description: string = '';
+        tags?: Array<string>;
+        deadline?: string;
+    }
+
+    export class ToDoItemDAO {
+        async listAll(): Promise<Array<ToDoItem>> {
+            const url = `${host}/${ra}/list`;
+
+            const response = await fetch(url);
+
+            if (response.ok) {
+                return (await response.json()).items as Array<ToDoItem>;
+            }
+
+            console.error('Server status: ' + JSON.stringify(await response.json()));
+            throw new Error("Server-side error. Failed to fetch list.");
+        }
+
+        insert(item: ToDoItem): boolean {
+            return false;
+        }
+    }
+}
+
+
+async function main() {
+    const dao = new Model.ToDoItemDAO();
+    
+    const result = await dao.listAll();
+
+    console.log(result);
+}
+
+main().then();
